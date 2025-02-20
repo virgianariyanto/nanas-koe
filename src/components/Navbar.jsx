421a7c66aa0fd1679c29c325fb98cb5de9eda0c1
Virgian Jaya Ariyanto <virgian.ariyanto@gmail.com>
2025-02-20 09:57:41 +0700

    Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
 
 const Navbar = () => {
     return (
@@ -12,11 +13,23 @@ const Navbar = () => {
                 <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
                     Best <span className="font-bold">Eats</span>
                 </h1>
-                <div>
-                    <p>Delivery</p>
-                    <p>Pickup</p>
+                <div className="hidden lg:flex items-center rounded-full p-1 bg-slate-200 text-[14px]">
+                    <p className="bg-black p-1 rounded-full text-white">Delivery</p>
+                    <p className="p-1">Pickup</p>
                 </div>
             </div>
+
+            {/* Search Input */}
+            <div className="flex items-center bg-slate-200 rounded-full px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+                <AiOutlineSearch size={20} className="text-black" />
+                <input type="text" className="p-2 focus:outline-none w-full text-slate-900" placeholder="search.." />
+            </div>
+
+            {/* Cart */}
+            <button className="bg-black text-white hidden md:flex items-center p-2 rounded-full">
+                <BsFillCartFill size={20} className="mr-1" /> Cart
+            </button>
+
         </div>
     )
 }
